796e61930a988a828564e2762920dc4eb1318655
Shatrughan Singh Bisen <84929529+Shatrughan-alt@users.noreply.github.com>
2024-12-27 14:17:56 +0530

    Update Login.jsx

--- front/src/Components/Login.jsx
+++ front/src/Components/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const url = "shatrughan-website.vercel.app/login";
+            const url = "https://shatrughan-website.vercel.app/login";
             const { data: res } = await axios.post(url, data);
             localStorage.setItem("token", res.data);
             window.location = "/";
