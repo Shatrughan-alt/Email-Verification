2c09f7c48ecc40e07726b6d4566b9095e258c9cd
Shatrughan Singh Bisen <84929529+Shatrughan-alt@users.noreply.github.com>
2024-12-27 20:25:32 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
-const mongoose=require('mongoose');
+// const mongoose=require('mongoose');
 
-mongoose.connect(process.env.DB)
-.then(()=>{
-    console.log("Connection Successful");
-}).catch(err=>console.log(err));
+// mongoose.connect(process.env.DB)
+// .then(()=>{
+//     console.log("Connection Successful");
+// }).catch(err=>console.log(err));
